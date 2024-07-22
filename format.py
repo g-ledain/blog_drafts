03f4e53f6368c27b69505377534ebff8a7a805b8
Gabriel Le Dain <153308331+g-ledain@users.noreply.github.com>
2024-07-22 18:32:15 +0100

    fixed formatting script

--- format.py
+++ format.py
@@ -19,42 +19,42 @@ def trailingContainsNewline(myString: str) -> bool:
         
 
 thisDir = os.path.dirname(os.path.realpath(__file__))
-for subdir, dirs, files in os.walk(os.path.join(thisDir,"unformatted")):
-    for file in files:
-        with open(os.path.join(thisDir,"unformatted",file),"r") as rawFile:
-            rawFileText = rawFile.read()
-        latexRegex = r"(?<!\\)\$\$|(?<!\\)\$"#matches text between single or double dollar signs, but doesn't match \$
-        #see https://stackoverflow.com/questions/35004800/regex-match-a-dollar-without-a-backslash-before-it
-        dollars=re.findall(latexRegex,rawFileText)
-        body=re.split(latexRegex,rawFileText)
-        parsed=body[0]
-        for i in range(len(dollars)):
-            dollarPart=dollars[i]
-            bodyPart = body[i+1]
-            newLine=""
-            mathMode=""
-            
-            if dollarPart=="$$":
-                mathMode="display"
-                tag="div"
-            if dollarPart=="$":
-                mathMode="inline"
-                tag="span"
-            
-            if i%2==0:#opening delimiter
-                if not trailingContainsNewline(body[i]) and mathMode=="display":
-                    newLine="\n"
-                parsed+=newLine
-                parsed+="<"+tag+">"
-                parsed+=dollarPart 
-            if i%2!=0:#closing delimiter
-                if not leadingContainsNewline(bodyPart) and mathMode=="display":
-                    newLine="\n"
-                parsed+=dollarPart
-                parsed+="</"+tag+">"
-                parsed+=newLine
-            parsed+=bodyPart
-            
-        with open(os.path.join(thisDir,"_posts",file),"w") as parsedFile:
-            parsedFile.write(parsed)
-            parsedFile.close()
+files = [f for f in os.listdir(os.path.join(thisDir,"unformatted")) if os.path.isfile(os.path.join(thisDir,"unformatted",f))]
+for file in files:
+    with open(os.path.join(thisDir,"unformatted",file),"r") as rawFile:
+        rawFileText = rawFile.read()
+    latexRegex = r"(?<!\\)\$\$|(?<!\\)\$"#matches text between single or double dollar signs, but doesn't match \$
+    #see https://stackoverflow.com/questions/35004800/regex-match-a-dollar-without-a-backslash-before-it
+    dollars=re.findall(latexRegex,rawFileText)
+    body=re.split(latexRegex,rawFileText)
+    parsed=body[0]
+    for i in range(len(dollars)):
+        dollarPart=dollars[i]
+        bodyPart = body[i+1]
+        newLine=""
+        mathMode=""
+        
+        if dollarPart=="$$":
+            mathMode="display"
+            tag="div"
+        if dollarPart=="$":
+            mathMode="inline"
+            tag="span"
+        
+        if i%2==0:#opening delimiter
+            if not trailingContainsNewline(body[i]) and mathMode=="display":
+                newLine="\n"
+            parsed+=newLine
+            parsed+="<"+tag+">"
+            parsed+=dollarPart 
+        if i%2!=0:#closing delimiter
+            if not leadingContainsNewline(bodyPart) and mathMode=="display":
+                newLine="\n"
+            parsed+=dollarPart
+            parsed+="</"+tag+">"
+            parsed+=newLine
+        parsed+=bodyPart
+        
+    with open(os.path.join(thisDir,"_posts",file),"w") as parsedFile:
+        parsedFile.write(parsed)
+        parsedFile.close()
